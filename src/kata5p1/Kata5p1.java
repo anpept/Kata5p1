447546361e7234fbda40c7a1e297f1a6b9f44866
Experimentos <anjupe@experimentos.com>
2018-11-15 18:11:21 +0000

    Kata 5 parte 1. Versión 1.

---
+++ src/kata5p1/Kata5p1.java
@@ -0,0 +1,10 @@
+package kata5p1;
+
+public class Kata5p1 {
+
+    
+    public static void main(String[] args) {
+        SelectApp app = new SelectApp();
+        app.selectAll();        
+    }    
+}
